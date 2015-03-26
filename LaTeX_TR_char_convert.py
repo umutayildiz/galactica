7a871e1bc0eb24e679b34749de3a1cf1bf9579ae
Umut Yildiz <yildiz@caltech.edu>
2015-03-25 17:10:24 -0700

    Create LaTeX_TR_char_convert.py

---
+++ LaTeX_TR_char_convert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+This little script converts Turkish characters in a file to LaTeX format
+"""
+
+
+import os,sys
+from chardet import detect
+import codecs
+
+# Just checking the encoding
+encoding = lambda x: detect(x)['encoding']
+
+print "-----------------------------------------------------------------"
+print "Please enter the name of the text file with Turkish characters..."
+print "-----------------------------------------------------------------"
+newtext = raw_input()
+if (newtext == ''):
+	print "No value entered, program exists!"
+	sys.exit()
+
+
+f=codecs.open(newtext,"r",encoding='utf-8')
+mytext=f.read()
+
+f=open("corr_"+newtext,"w")
+f.write(mytext.encode('utf-8').replace('ı', '{\i}').replace('İ','\\.{I}').replace('ğ','\\u{g}').replace('Ğ','\\u{G}').replace('ç','\\c{c}').replace('Ç','\\c{C}').replace('ş','\\c{s}').replace('Ş','\\c{S}').replace('ü','\\"{u}').replace('Ü','\\"{U}').replace('ö','\\"{o}').replace('Ö','\\"{O}'))
+f.close
+
+print "-----------------------------------------------------------------------"
+print '>>> SUCCESS! LaTeX version of the text is now called "corr_'+newtext+'"'
+print "-----------------------------------------------------------------------"
